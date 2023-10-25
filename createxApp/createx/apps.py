5af00f1f68c761ae5c9b65bb4ccb18767a5061d3
Denis <denis.d88@bk.ru>
2023-10-25 12:45:09 +0700

    коммит 2 - приложение createx и таблица стилей

---
+++ createxApp/createx/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class CreatexConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'createx'
+    verbose_name = 'Строительная компания Createx'
--- createxApp/createxApp/settings.py
+++ createxApp/createxApp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Приложения django
+    'createx.apps.CreatexConfig',#Приложение createx
+
+    #Дополнительные приложения
+    'bootstrap4',  # бутстрап 4
 ]
 
 MIDDLEWARE = [
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'createxApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
@@ -115,7 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+#Статические файлы - js, css, img и тд. - static в главной директории
+STATIC_URL = 'static/' #префикс url - адреса статических файлов
+STATIC_ROOT = [os.path.join(BASE_DIR, 'static')] # путь к общей папке для веб-сервера
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+#Настройка медиа - для добавления фотографий для моделей
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
